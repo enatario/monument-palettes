403e7ab0b34b4eb766d8e189d60a9c5fc04e7ad7
Elaina Natario <elaina.natario@gmail.com>
2021-01-15 16:35:22 -0500

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addWatchTarget("./src/js/");
+  eleventyConfig.addPassthroughCopy("./src/static");
+
+  eleventyConfig.addFilter("keys", obj => Object.keys(obj));
+  eleventyConfig.addFilter("except", (arr=[]) => {
+    return arr.filter(function(value) {
+      return value != "all";
+    }).sort();
+  });
+  
+  return {
+    templateFormats: [
+      "md",
+      "njk"
+    ],
+
+    dir: {
+      input: "src",
+      output: "dist"
+    },
+    markdownTemplateEngine: "njk"
+  }
+}
